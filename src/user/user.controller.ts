15417771b1e8660256f8ab02f10de7c46162d192
Lukazx15 <nattananpunpetch@gmail.com>
2026-03-22 14:47:55 +0700

    test05

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
-
-
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
