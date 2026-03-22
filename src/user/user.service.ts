b81ec30385999c6fb36409750f9c3a551c495dcc
Lukazx15 <nattananpunpetch@gmail.com>
2026-03-22 22:57:44 +0700

    test

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { IUser } from './user.interface';
 import * as fs from 'fs';
 import * as path from 'path';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
@@ -12,5 +13,24 @@ export class UserService {
     const filePath = path.join(process.cwd(), 'data', 'users.json');
     const data = fs.readFileSync(filePath, 'utf-8');
     return JSON.parse(data) as IUser[];
+  }
+  findOne(id: string, fields?: string[]): Partial<IUser> {
+    const users = this.findAll();
+
+    const user = users.find((u) => u.id === id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (!fields || fields.length === 0) {
+      return user;
+    }
+    const result: Partial<IUser> = {};
+    for (const field of fields) {
+      if (field in user) {
+        result[field as keyof IUser] = user[field as keyof IUser];
+      }
+    }
+    return result;
   }
 }
