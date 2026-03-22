f504285727c50eafc43292342591b82c3b9f78be
Lukazx15 <nattananpunpetch@gmail.com>
2026-03-22 14:56:37 +0700

    test07

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,7 +5,7 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('user')
+  @Get('test')
   user(): string[] {
     return this.userService.test();
   }
